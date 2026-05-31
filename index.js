4a2fe68ff41737a88b93a6b1469d9cd9b6ba6928
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-06-01 01:42:03 +0600

    POST method practiced

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World from TR TECH!')
@@ -17,9 +23,30 @@ app.get('/users', (req, res) => {
     res.send(users);
 })
 
+app.post('/users', (req, res) => {
+    console.log(req.headers['content-type']);
+    console.log("Data in request:", req.body);
+
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+
+    res.send({
+        success: true,
+        data: newUser,
+        message: "User created successfully",
+        user: req.body
+    });
+});
+
+
+
+
 app.get('/products', (req, res) => {
     res.send("Products are selling on market")
 })
+
+
 
 
 
